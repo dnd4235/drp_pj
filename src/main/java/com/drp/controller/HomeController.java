b5987d2eca322c4f09f65b94762fed01c76c7d88
ram <ram@DESKTOP-AETA3LF>
2018-08-10 11:01:47 +0900

    test commit

--- src/main/java/com/drp/controller/HomeController.java
+++ src/main/java/com/drp/controller/HomeController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.drp.service.TestService;
 
 /**
- * Handles requests for the application home page.
+ * Handles requests for the application home page. -
  */
 @Controller
 public class HomeController {
